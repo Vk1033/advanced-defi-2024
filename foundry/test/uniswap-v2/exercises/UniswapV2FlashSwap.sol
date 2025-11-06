1862c1c802ad777850b583d5f1c57ba3d036d38d
Vk <kinkarvansh@gmail.com>
2025-11-06 06:57:58 +0000

    feat: implement flash swap logic and validation in UniswapV2FlashSwap

--- foundry/test/uniswap-v2/exercises/UniswapV2FlashSwap.sol
+++ foundry/test/uniswap-v2/exercises/UniswapV2FlashSwap.sol
@@ -28,11 +28,17 @@ contract UniswapV2FlashSwap {
 
         // 1. Determine amount0Out and amount1Out
         (uint256 amount0Out, uint256 amount1Out) = (0, 0);
+        if (token == token0) {
+            amount0Out = amount;
+        } else {
+            amount1Out = amount;
+        }
 
         // 2. Encode token and msg.sender as bytes
-        bytes memory data;
+        bytes memory data = abi.encode(token, msg.sender);
 
         // 3. Call pair.swap
+        pair.swap(amount0Out, amount1Out, address(this), data);
     }
 
     // Uniswap V2 callback
@@ -46,23 +52,39 @@ contract UniswapV2FlashSwap {
         // Don’t change any other code
 
         // 1. Require msg.sender is pair contract
+        if (msg.sender != address(pair)) {
+            revert InvalidToken();
+        }
         // 2. Require sender is this contract
+        if (sender != address(this)) {
+            revert InvalidToken();
+        }
         // Alice -> FlashSwap ---- to = FlashSwap ----> UniswapV2Pair
         //                    <-- sender = FlashSwap --
         // Eve ------------ to = FlashSwap -----------> UniswapV2Pair
         //          FlashSwap <-- sender = Eve --------
 
         // 3. Decode token and caller from data
-        (address token, address caller) = (address(0), address(0));
+        (address token, address caller) = abi.decode(data, (address, address));
         // 4. Determine amount borrowed (only one of them is > 0)
-        uint256 amount = 0;
+
+        uint256 amount;
+        if (amount0 > 0) {
+            amount = amount0;
+        } else {
+            amount = amount1;
+        }
 
         // 5. Calculate flash swap fee and amount to repay
         // fee = borrowed amount * 3 / 997 + 1 to round up
-        uint256 fee = 0;
-        uint256 amountToRepay = 0;
+        uint256 fee = (amount * 3) / 997 + 1;
+        uint256 amountToRepay = amount + fee;
 
         // 6. Get flash swap fee from caller
+        uint256 callerFee = IERC20(token).balanceOf(caller);
+        require(callerFee >= fee, "Insufficient fee");
+        IERC20(token).transferFrom(caller, address(this), fee);
         // 7. Repay Uniswap V2 pair
+        IERC20(token).transfer(address(pair), amountToRepay);
     }
 }
